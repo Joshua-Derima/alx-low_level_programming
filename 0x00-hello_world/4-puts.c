b5b0e3438abdd0c901fa6baaef6fa00b5ab1c943
root <derima45@gmail.com>
2022-09-08 08:41:53 -0700

    printing values on C with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-/* experimenting with puts function*/
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle\n");
 	return (0);
 }
 
